e8fb9ef2f6d02b35c4f2049d35dab2a12acec0c1
libbyschuknight <libbysk@gmail.com>
2015-11-01 08:24:10 +1300

    Get the coffee example working

--- scripts/lunch.js
+++ scripts/lunch.js
@@ -1,21 +1,31 @@
-if (process.env.NODE_ENV !== "production") { require("dotenv").load() }
+if (process.env.NODE_ENV !== 'production') { require('dotenv').load() }
 
-var _ = require("lodash")
-var foursquare = require("node-foursqaure-venues")(process.env.FOURSQUARE_CLIENT_ID,
-                                  process.env.FOURSQURE_CLIENT_SECRET)
+var _ = require('lodash')
+var foursquare = require('node-foursquare-venues')(process.env.FOURSQUARE_CLIENT_ID, process.env.FOURSQUARE_CLIENT_SECRET)
 
-mdoule.exports = function (bot) {
+module.exports = function (bot) {
   bot.respond(/coffee near (.*)/i, function (res) {
     var params = {
       near: res.match[1],
-      categoryId: "4bf58dd8d48988d1e0931735",
+      categoryId: '4bf58dd8d48988d1e0931735',
       radius: 1000
     }
 
     return foursquare.venues.search(params, function (error, payload) {
-      if (error) resturn res.send(error)
+      if (error) return res.send(error)
 
       var message = '\nHello ! I\'m the neohipster coffee bot. You have this coffee shops nearby:\n\n'
+
+      var coffeeShops = _.sample(payload.response.venues, 10)
+      var url = 'https://www.foursquare.com/v/'
+
+      for (var i = 0; i < coffeeShops.length; i++) {
+        var cs = coffeeShops[i]
+        message += i + ': ' + cs.name + ' (' + url + cs.id + ')\n'
+        message += cs.location.address + '\n\n'
+      }
+
+      return res.send(message)
     })
-  }
-)}
+  })
+}
